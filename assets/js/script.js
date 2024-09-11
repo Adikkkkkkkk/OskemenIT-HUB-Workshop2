dd335ec41c400c08a57d107c5331ff6cc38a00c2
Adikkkkkkkk <raximov_adil@mail.ru>
2024-09-11 19:51:47 +0500

    Created tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,60 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.style.backgroundColor = "green";
+window.addEventListener('DOMContentLoaded', () => {
+	let tabs = [
+		{
+			id: 'audi',
+			brand: 'Audi',
+		},
+		{
+			id: 'bmw',
+			brand: 'BMW',
+		},
+		{
+			id: 'hyundai',
+			brand: 'Hyundai',
+		},
+		{
+			id: 'kia',
+			brand: 'Kia',
+		},
+		{
+			id: 'land-rover',
+			brand: 'Land Rover',
+		},
+		{
+			id: 'mercedes',
+			brand: 'Mercedes',
+		},
+		{
+			id: 'nissan',
+			brand: 'Nissan',
+		},
+		{
+			id: 'toyota',
+			brand: 'Toyota',
+		},
+	];
+
+	const tabsEl = document.querySelector('.tab-buttons');
+
+	// Tabs
+	tabs.forEach(tab => {
+		tabsEl.innerHTML += `
+		<button class='tab-button' id='${tab.id}'><i class='bi bi-car-front-fill'></i> ${tab.brand}</button>
+		`;
+	});
+
+	const tabButtons = document.querySelectorAll('.tab-button');
+
+	tabButtons.forEach(tabButton => {
+		tabButton.addEventListener('click', function (e) {
+			e.preventDefault();
+
+			let brand = this.textContent;
+			console.log(brand);
+		});
+	}); /* ForEach */
+
+	// Cards
 }); /* DOMContentLoaded */
